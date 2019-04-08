dde98d26ffc9ab817d635aca08b7cb3df18232e6
Scott Carroll <scott_carroll19@yahoo.com>
2019-04-07 20:45:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas, ctx, center_x, center_y, radius = 100, bars = 600, x_end, y_end, bar_height, bar_width = 3, frequency_array;
+var canvas, ctx, center_x, center_y, radius = window.innerHeight / 6.25, bars = 600, x_end, y_end, bar_height, bar_width = 4, frequency_array;
 function initPage() {
   var song = document.getElementById("song").value;
   audio = document.getElementById("audio");
@@ -65,17 +65,17 @@ function animationLooper(){
   analyser.getByteFrequencyData(frequency_array);
   for(var i = 0; i < bars; i++){
     rads = Math.PI * 2 / bars;
-    bar_height = frequency_array[i]*2;
+    bar_height = Math.pow(frequency_array[i] / 255 * Math.pow((window.innerHeight - (radius / 2)), 1/5), 5);
     x = center_x + Math.cos(rads * i) * (radius);
     y = center_y + Math.sin(rads * i) * (radius);
     x_end = center_x + Math.cos(rads * i)*(radius + bar_height);
     y_end = center_y + Math.sin(rads * i)*(radius + bar_height);
-    drawBar(x, y, x_end, y_end, bar_width, frequency_array[i]);
+    drawBar(x, y, x_end, y_end, bar_width);
   }
   if (audio.ended == false) {window.requestAnimationFrame(animationLooper);}
 }
-function drawBar(x1, y1, x2, y2, width, frequency){
-  var lineColor = "rgb(" + 255 + ", " + frequency*2 + ", " + 0 + ")";
+function drawBar(x1, y1, x2, y2, width){
+  var lineColor = "rgb(" + 255 + ", " + 255 + ", " + 0 + ")";
   ctx.strokeStyle = lineColor;
   ctx.lineWidth = width;
   ctx.beginPath();
