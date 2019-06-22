a27c34329a73a769ff5dd2c3aa0f298a9944c7fe
Scott Carroll <scott_carroll19@yahoo.com>
2019-06-22 11:59:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function initPage() {
   ctx.arc(center_x, center_y, radius, 0, 2*Math.PI);
   ctx.stroke();
   analyser.fftSize = 4096;
-  analyser.smoothingTimeConstant = 0.2;
+  analyser.smoothingTimeConstant = 0;
   analyser.minDecibels = -75;
-  analyser.maxDecibels = 0;
+  analyser.maxDecibels = -10;
   analyser.getByteFrequencyData(frequency_array);
   document.getElementById("songSelectMenu").style.visibility = "hidden";
   setTimeout(function() {
