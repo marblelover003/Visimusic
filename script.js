9bcc35f098bcca1ff68712ccda473f32f51707f3
Scott Carroll <scott_carroll19@yahoo.com>
2019-07-21 17:57:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas, ctx, center_x, center_y, radius = 120, bars = 500, x_end, y_end, bar_height, bar_width = 2, frequency_array;
+var canvas, ctx, center_x, center_y, radius = 150, bars = 500, x_end, y_end, bar_height, bar_width = 2, frequency_array;
 function initPage() {
   var song = document.getElementById("song").value;
   audio = document.getElementById("audio");
@@ -61,9 +61,9 @@ function animationLooper(){
   analyser.getByteFrequencyData(frequency_array);
   for(var i = 0; i < bars; i++){
     rads = Math.PI * 2 / bars;
-    bar_height = Math.pow(frequency_array[i] / 255 * Math.pow(240, 1/4), 4);
-    x = center_x + Math.cos(rads * i) * (radius - (bar_height / 2));
-    y = center_y + Math.sin(rads * i) * (radius - (bar_height / 2));
+    bar_height = Math.pow(frequency_array[i] / 255 * Math.pow(150, 1/4), 4);
+    x = center_x + Math.cos(rads * i) * (radius - bar_height);
+    y = center_y + Math.sin(rads * i) * (radius - bar_height);
     x_end = center_x + Math.cos(rads * i)*(radius + bar_height);
     y_end = center_y + Math.sin(rads * i)*(radius + bar_height);
     drawBar(x, y, x_end, y_end, bar_width);
